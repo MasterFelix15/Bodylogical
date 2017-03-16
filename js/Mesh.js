a308174662542947dfbffa1ec01ddbc900daa8fa
Master Felix <felixatnus2016@gmail.com>
2017-03-15 17:46:04 -0700

    Added grid lines

--- js/Mesh.js
+++ js/Mesh.js
@@ -2,7 +2,10 @@
  * Created by felix on 31/1/17.
  */
 
-var plane, ball, planeGeo, ballGeo;
+var plane;
+var ball;
+var planeGeo;
+var ballGeo;
 var planeWidth = 10;
 var planeLength = 50;
 var trenchWidth = 20;
@@ -17,20 +20,57 @@ var sx = 70;
 var bally = 2;
 var ballRadius = 0.5;
 var tmax = 3;
+// variables to control the sinking of trench
+var steadyTime = 2;
+var maxTime = 5;
+var trenches = [65, 70];
 var t = 2;
-var wireFrameOn = true;
+var wireFrameOn = false;
 var group;
 
+var lineStartY = 1;
+var lineOffset = 2;
+var BMILineNum = 20;
+var BMILineIndexOffset = grid/lineOffset;
+var lineGeos = [];
+
+var lastframeX = sx;
+
+function Trench(center, min, max, depth) {
+    this.center = center;
+    this.min = min;
+    this.max = max;
+    this.depth = depth;
+}
+
+function addTrench(center) {
+    var newTrench = new Trench(center, center-10, center+10, c);
+    var trenchIndex = 0;
+    for (var i = 0; i < trenches.length-1; i++) {
+        if (trenches[i].center <= center && trenches[i+1].center >= center) {
+            // this is where the new trench belongs
+            trenchIndex = i+1;
+            break;
+        }
+    }
+    if (center > trenches[trenches.length-1].center) {
+
+    }
+
+}
+
 function drawTrench(sx, firstTime) {
-    for (var i = 0; i < planeGeo.vertices.length; i+=(grid+1)) {
+    var ageLineOffset = Math.sqrt((sx-lastframeX)/d);
+    console.log(ageLineOffset);
+    for (var i = planeGeo.vertices.length - grid - 1; i >= 0; i-=(grid+1)) {
         for (var j = i; j < i+(grid+1); j++) {
             var x = j - i;
-            var y = grid - i / (grid+1);
+            var y = parseInt(grid - i / (grid+1));
             var z = a*Math.pow(x,2) + b*Math.pow(y,2);
             var cur_sx = d*Math.pow(y,2) + sx;
             if (x >= cur_sx - trenchWidth/2 && x <= cur_sx + trenchWidth/2) {
-                var theta = (x - cur_sx)/-(trenchWidth/2)*Math.PI;
-                z += c*(Math.cos(theta)+1)*t/tmax;
+                var theta = (x - cur_sx) / -(trenchWidth / 2) * Math.PI;
+                z += c * (Math.cos(theta) + 1) * t / tmax;
             }
             if (x == parseInt(cur_sx) && y == bally) {
                 ballx = cur_sx;
@@ -38,6 +78,49 @@ function drawTrench(sx, firstTime) {
             }
             if (firstTime) {
                 planeGeo.vertices[j].y += (planeLength - planeWidth)/2;
+            }
+            planeGeo.vertices[j].z = z;
+
+            // process frame line offset
+
+            if ((y-lineStartY)%lineOffset == 0) {
+                lineGeos[(y-lineStartY)/lineOffset].vertices[x] = new THREE.Vector3 (
+                    planeGeo.vertices[j].x,
+                    planeGeo.vertices[j].y,
+                    planeGeo.vertices[j].z + 0.01);
+            }
+
+            if (x%(grid/BMILineNum) == 0) {
+                lineGeos[BMILineIndexOffset + x/(grid/BMILineNum)].vertices[y] = new THREE.Vector3 (
+                    planeGeo.vertices[j].x,
+                    planeGeo.vertices[j].y,
+                    planeGeo.vertices[j].z + 0.01);
+            }
+
+        }
+    }
+    lastframeX = sx;
+}
+
+function drawMultipleTrenches(sx, firstTime) {
+    for (var i = 0; i < planeGeo.vertices.length; i+=(grid+1)) {
+        for (var j = i; j < i+(grid+1); j++) {
+            var x = j - i;
+            var y = grid - i / (grid+1);
+            var z = a*Math.pow(x,2) + b*Math.pow(y,2);
+            for (var k = 0; k < trenches.length; k++) {
+                var cur_sx = d * Math.pow(y, 2) + sx[k];
+                if (x >= cur_sx - trenchWidth / 2 && x <= cur_sx + trenchWidth / 2) {
+                    var theta = (x - cur_sx) / -(trenchWidth / 2) * Math.PI;
+                    z += c * (Math.cos(theta) + 1) * t / tmax;
+                }
+                if (x == parseInt(cur_sx) && y == bally) {
+                    ballx = cur_sx;
+                    ballz = a * Math.pow(ballx, 2) + b * Math.pow(bally, 2) + 2 * c * t / tmax + ballRadius;
+                }
+            }
+            if (firstTime) {
+                planeGeo.vertices[j].y += (planeLength - planeWidth) / 2;
             }
             planeGeo.vertices[j].z = z;
         }
@@ -51,63 +134,91 @@ function putBall() {
 }
 
 function produceMeshGroup() {
-    planeGeo = new THREE.PlaneGeometry( planeWidth, planeLength, grid, grid );
-    planeGeo.dynamic = true;
 
-    drawTrench(sx, true);
+    /////////////////////////////////////////////////
+    var ageLineMaterial = new THREE.LineBasicMaterial({
+        color: 0xFDFEFE,
+        linewidth: 2,
+        fog:true
+    });
+
+    var BMILineMaterial = new THREE.LineBasicMaterial({
+        color: 0xEC7063,
+        linewidth: 2,
+        fog:true
+    });
 
     var planeMaterial = new THREE.MeshPhongMaterial({
-        color: 0x6699CC,
-        side: THREE.DoubleSide,
+        color: 0x85C1E9,
         wireframe: wireFrameOn,
-        shading: THREE.flatShading,
-        shininess: 20
+        side: THREE.DoubleSide
     });
-    plane =  new THREE.Mesh( planeGeo, planeMaterial );
+
+    var ballMaterial = new THREE.MeshPhongMaterial({
+        color: 0xE8F8F5,
+        wireframe: wireFrameOn,
+        shading: THREE.SmoothShading,
+        shininess: 0
+    });
+    /////////////////////////////////////////////////
 
 
     group = new THREE.Group();
+
+    planeGeo = new THREE.PlaneGeometry( planeWidth, planeLength, grid, grid );
+    planeGeo.dynamic = true;
+
+    // create line geometries to represent age
+    for (var i = 0; i < grid/lineOffset; i ++) {
+        var geometry = new THREE.Geometry();
+        geometry.dynamic = true;
+        for (var j = 0; j <= grid; j++) {
+            geometry.vertices.push( new THREE.Vector3( 0, 0, 0 ));
+        }
+        lineGeos.push(geometry);
+    }
+
+    // create line geometries to represent BMI
+    for (var i = 0; i <= BMILineNum; i ++) {
+        var geometry = new THREE.Geometry();
+        geometry.dynamic = true;
+        for (var j = 0; j <= grid; j++) {
+            geometry.vertices.push( new THREE.Vector3( 0, 0, 0 ));
+        }
+        lineGeos.push(geometry);
+    }
+
+    console.log(lineGeos.length);
+
+    drawTrench(sx, true);
+    //drawMultipleTrenches(trenches, true);
+
+
+    plane =  new THREE.Mesh( planeGeo, planeMaterial );
     group.add(plane);
 
+
+    for (var i = 0; i < lineGeos.length; i++) {
+        if (i < BMILineIndexOffset) {
+            group.add( new THREE.Line (lineGeos[i], ageLineMaterial));
+        } else {
+            group.add(new THREE.Line(lineGeos[i], BMILineMaterial));
+        }
+    }
+
     ballGeo = new THREE.SphereGeometry( ballRadius, ballComplexity,  ballComplexity);
-
-    var ballMaterial = new THREE.MeshPhongMaterial({
-        color: 0x99FFCC,
-        wireframe: wireFrameOn,
-        shading: THREE.SmoothShading,
-        shininess: 500
-    });
-
     ball = new THREE.Mesh( ballGeo, ballMaterial);
-
     group.add(ball);
     putBall();
-
-    var materialy = new THREE.LineBasicMaterial({ color: 0x138D75 });
-
-    var yaxisGeo = new THREE.Geometry();
-    yaxisGeo.vertices.push( new THREE.Vector3( -5, -5, 0 ), new THREE.Vector3( -5, 5, 0 ) );
-    var yaxis = new THREE.Line( yaxisGeo, materialy );
-    group.add( yaxis );
-
-    var materialx = new THREE.LineBasicMaterial({ color: 0xFF0033 });
-
-    var xaxisGeo = new THREE.Geometry();
-    xaxisGeo.vertices.push( new THREE.Vector3( -5, -5, 0 ), new THREE.Vector3( 5, -5, 0 ) );
-    var xaxis = new THREE.Line( xaxisGeo, materialx );
-    group.add( xaxis );
-
-    var materialz = new THREE.LineBasicMaterial({ color: 0x2471A3 });
-
-    var zaxisGeo = new THREE.Geometry();
-    zaxisGeo.vertices.push( new THREE.Vector3( -5, -5, 0 ), new THREE.Vector3( -5, -5, 10 ) );
-    var zaxis = new THREE.Line( zaxisGeo, materialz );
-    group.add( zaxis );
 
 }
 
 function updateAll(cur_sx) {
     planeGeo.verticesNeedUpdate = true;
+    for (var i = 0; i < lineGeos.length; i++) {
+        lineGeos[i].verticesNeedUpdate = true;
+    }
+
     drawTrench(cur_sx, false);
     putBall();
 }
